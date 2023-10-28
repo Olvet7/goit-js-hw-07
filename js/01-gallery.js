651d0f6775f302814ff3cf293ca247ce31ff5b5c
Olvet7 <volya.maslova@gmail.com>
2023-10-28 03:06:05 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// Доступ до пустої галереї в html
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList);
+
+// Функція, яка повертає рядок із всіма картинками 
+// description, original,  preview - структура однієї картинки з галереї
+
+function galleryMarcupItem(items) {
+    return items
+    .map(({description, original, preview}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="Image description"">
+        </a>
+      </li>
+        `;
+    })
+   .join('');
+}
+
+// Створюємо всі картки функцією galleryMarcupItem
+const galleryMarcup = galleryMarcupItem(galleryItems);
+
+// Додаємо в DOM всі картки
+galleryList.innerHTML = galleryMarcup;
+
+// Додаємо слухача на список галереї
+galleryList.addEventListener("click", onImageClick);
+
+// Функція відкрити картинки
+function onImageClick(event) {
+
+// відміна дефолтого нвідкриття за посиланням 
+    event.preventDefault();
+if (event.target.tagName !== "IMG") {
+    return
+} 
+
+// відкриття великої картинки
+const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+instance.show()
+
+// Функція закриття картинки
+galleryList.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+  });
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Доступ до пустої галереї в html
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList);
+
+// Функція, яка повертає рядок із всіма картинками 
+// description, original,  preview - структура однієї картинки з галереї
+
+function galleryMarcupItem(items) {
+    return items
+    .map(({description, original, preview}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>
+        `;
+    })
+   .join('');
+}
+
+// Створюємо всі картки функцією galleryMarcupItem
+const galleryMarcup = galleryMarcupItem(galleryItems);
+
+// Додаємо в DOM всі картки
+galleryList.innerHTML = galleryMarcup;
+
+// Слухач події з функцією гортання картинки вправо-вліво і виведення підпису із alt знизу картинки, із затримкою 250 мілісекунд
+document.addEventListener('DOMContentLoaded', function () { const gallery = new SimpleLightbox('.gallery a', { captions: true, captionType: 'attr', captionsData: 'alt', captionDelay: 250, }); });
